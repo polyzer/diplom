727c14a89f71e11fb79b9973615e213bfc3e8f08
polyzer <realpolyzer@yandex.ru>
2020-02-15 06:41:09 +0500

    update

--- generate_datasets.py
+++ generate_datasets.py
@@ -1,7 +1,6 @@
 import h5py, os
 import caffe
 import numpy as np
-import lmdb
 import matplotlib.pyplot as plt
 from caffe.proto import caffe_pb2
 from caffe.io import datum_to_array, array_to_datum
@@ -9,69 +8,25 @@ import glob
 import pandas as pd
 import argparse
 
-IMG_SIZE = 224
+IMG_SIZE = 200
 
-def write_images_to_lmdb(img_dir, db_name):
-    for root, dirs, files in os.walk(img_dir, topdown = False):
-        if root != img_dir:
-            continue
-        map_size = IMG_SIZE*IMG_SIZE*3*2*len(files)
-        env = lmdb.Environment(db_name, map_size=map_size)
-        txn = env.begin(write=True,buffers=True)
-        for idx, name in enumerate(files):
-            X = mp.imread(os.path.join(root, name))
-            y = 1
-            datum = array_to_datum(X,y)
-            str_id = '{:08}'.format(idx)
-            txn.put(str_id.encode('ascii'), datum.SerializeToString())   
-    txn.commit()
-    env.close()
-    print " ".join(["Writing to", db_name, "done!"])
 
-def read_images_from_lmdb(db_name, visualize):
-	env = lmdb.open(db_name)
-	txn = env.begin()
-	cursor = txn.cursor()
-	X = []
-	y = []
-	idxs = []
-	for idx, (key, value) in enumerate(cursor):
-		datum = caffe_pb2.Datum()
-		datum.ParseFromString(value)
-		X.append(np.array(datum_to_array(datum)))
-		y.append(datum.label)
-		idxs.append(idx)
-	if visualize:
-	    print "Visualizing a few images..."
-	    for i in range(9):
-	        img = X[i]
-	        plt.subplot(3,3,i+1)
-	        plt.imshow(img)
-	        plt.title(y[i])
-	        plt.axis('off')
-	    plt.show()
-	print " ".join(["Reading from", db_name, "done!"])
-	return X, y, idxs
+def generate_h5_dataset(train_files):
 
-def generate_h5_dataset(imgs_folder):
-    SIZE = 224 # fixed size to all images
-    with open( 'train.txt', 'r' ) as T :
-        lines = T.readlines()
+    df = pd.read_csv(train_files)
+    print(df.head())
+    print(df.shape)
+    #exit()
     # If you do not have enough memory split data into
     # multiple batches and generate multiple separate h5 files
-    X = np.zeros( (len(lines), SIZE, SIZE, 3), dtype='f4' ) 
-    y = np.zeros( (len(lines), 1), dtype='f4' )
-    for i,l in enumerate(lines):
-        sp = l.split(' ')
-        img = np.load('/tmp/123.npy')
-        #img = caffe.io.load_image( sp[0] )
-        #img = caffe.io.resize( img, (SIZE, SIZE, 3) ) # resize to fixed size
-        # you may apply other input transformations here...
-        # Note that the transformation should take img from size-by-size-by-3 and transpose it to 3-by-size-by-size
-        # for example
-        # transposed_img = img.transpose((2,0,1))[::-1,:,:] # RGB->BGR
-        #X[i] = transposed_img
-        y[i] = float(sp[1])
+    X = np.zeros( (df.shape[0], IMG_SIZE, IMG_SIZE), dtype='f4' ) 
+    y = np.zeros( (df.shape[0], 1), dtype='f4' )
+    for index, row in df.iterrows():
+        label = row["label"]
+        f_name = row["filenames"]
+        img = np.load(f_name)
+        y[index] = int(label)
+        X[index] = img
     with h5py.File('train.h5','w') as H:
         H.create_dataset( 'X', data=X ) # note the name X given to the dataset!
         H.create_dataset( 'y', data=y ) # note the name y given to the dataset!
@@ -79,22 +34,29 @@ def generate_h5_dataset(imgs_folder):
         L.write( 'train.h5' ) # list all h5 files you are going to use
 
 
-def generate_labeling_textfile(in_dir, save_dir, save_file_name):
+def generate_labeling_textfile(in_dir, save_file_name):
     # f_filenames = glob.glob(os.path.join(in_dir, "F*.npy"))
     # m_filenames = glob.glob(os.path.join(in_dir, "M*.npy"))
     f_names = glob.glob(os.path.join(in_dir, "*.npy"))
-    save_file = open(os.path.join(save_dir, save_file_name), "w")
+    save_file = open(os.path.join(save_file_name), "w")
     counter = 0
     df = pd.DataFrame({
         "filenames": f_names
     })
-    df.to_csv(save_file_name)
+    df.to_csv(save_file_name, index_label="label")
     # for idx, f_name in enumerate(f_filenames):
     #     file_name = 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='dataset generation file')
-    parser.add_argument('store_path',  help='NPY files directory')
-    parser.add_argument('save_name',  help='File name to save data')
-    
-    
+    parser.add_argument('--type', type=int, help='File name to save data')
+    parser.add_argument('--npy_dir', type=str,  help='NPY files directory')
+    parser.add_argument('--save_name', type=str, help='File name to save data')
+    parser.add_argument('--train_name', type=str, help='File to load from')
+
+    args = parser.parse_args()
+
+    if args.type == 1:
+        generate_labeling_textfile(args.npy_dir, args.save_name)
+    if args.type == 2:
+        generate_h5_dataset(args.train_name)
